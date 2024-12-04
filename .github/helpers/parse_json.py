7aa2880cb4bd9d3b8b548a1da5605c684007e10f
dulude <dulude@stsci.edu>
2024-12-04 15:54:37 -0500

    fleshed out parse_json.py

--- .github/helpers/parse_json.py
+++ .github/helpers/parse_json.py
@@ -1,13 +1,38 @@
+import argparse
 import json
-import json_repair #will have to pip install
 import pdb
+import sys
 
-json_filename = 'barf1.json'
+import json_repair #will have to pip install
 
-# Fix any json formatting errors
-json_object = json_repair.from_file(json_filename)
+def parse_json_file(json_filename):
+    """reads through json file produced by a11ywatch API and displays the results.
 
-# pretty print json filecld
-json_formatted_str = json.dumps(json_object, indent=2)
-print(json_formatted_str)
-pdb.set_trace()
+    Parameters
+    ----------
+    json_filename : str
+        name of the json file to process
+
+    Returns
+    -------
+    rv : int
+        if a11ywatch didn't find any issues, rv will be integer value '0'. However, if a11ywatch did find
+        issues, rv will be integer value '99'.
+    """
+    # Fix any json formatting errors
+    json_object = json_repair.from_file(json_filename)
+
+    # pretty print json file contents
+    json_formatted_str = json.dumps(json_object, indent=2)
+    print(json_formatted_str)
+    rv = 0
+    return rv
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument( 'json_filename', type=str, help='Name of the json file to be processed')
+    args = parser.parse_args()
+
+    rv = parse_json_file(args.json_filename)
+    sys.exit(rv)
